6fe5fbbeeb32e764e8c499fed212d9ae5dce348e
nicolas cros <none>
2021-03-26 07:56:48 +0100

    add two URI to control a gpio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("192.168.1.22:7878").unwrap();
@@ -6,6 +8,27 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 10244];
+    stream.read(&mut buffer).unwrap();
+
+    let set_gpio = b"GET /gpio_1 HTTP/1.1\r\n";
+    let reset_gpio = b"GET /gpio_0 HTTP/1.1\r\n";
+
+    if buffer.starts_with(set_gpio) {
+        println!("Set gpio.");
+    } 
+
+    if buffer.starts_with(reset_gpio) {
+        println!("Reset gpio.");
+    } 
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
